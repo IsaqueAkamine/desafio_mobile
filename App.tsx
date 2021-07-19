fc73555c9091338fe7c37a876c2e0e9180738f45
Isaque Akamine <isaque.akamine@MBP-Condomob-0005.local>
2021-07-19 14:32:39 -0300

    adjust configs new app

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import Navigation from './src/navigation';
 
 import * as firebase from 'firebase';
-import { firebaseConfig } from './src/utils/fireConfig';
+import { firebaseConfig } from './src/utils/config';
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
